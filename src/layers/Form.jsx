d4777bf19eb7bbd6e150e1180b383f66f61bb161
Maxime <maxime.lombardo@gmail.com>
2020-01-29 15:01:11 +0100

    composant créés

--- src/layers/Form.jsx
+++ src/layers/Form.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+import "./Form.css";
+
 export default function Form() {
-  return <div></div>;
+  return (
+    <div className='form-container'>
+      <h1>CONTACT</h1>
+      <form className='form'>
+        <div className='form-lign'>
+          <label for='lastname'>Nom :</label>
+          <input name='lastname' required></input>
+        </div>
+        <div className='form-lign'>
+          <label for='firstname'>Prénom :</label>
+          <input name='firstname' required></input>
+        </div>
+        <div className='form-lign'>
+          <label for='email'>Email :</label>
+          <input type='email' name='email' required></input>
+        </div>
+        <div className='form-lign'>
+          <label for='text'>Votre message :</label>
+          <textarea name='text'></textarea>
+        </div>
+      </form>
+      <button className='button'>Envoyer</button>
+    </div>
+  );
 }
