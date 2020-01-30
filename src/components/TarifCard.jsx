b1746686903bcdc81317d91fb09f2052d6d60442
Maxime <maxime.lombardo@gmail.com>
2020-01-30 13:20:49 +0100

    post réussi sur la table user_has...

--- src/components/TarifCard.jsx
+++ src/components/TarifCard.jsx
@@ -5,23 +5,26 @@ import "./TarifCard.css";
 
 export default function TarifCard(props) {
   const [prix, setPrix] = useState("");
+  const [ok, setOk] = useState(false);
 
   const handleClick = () => {
+    setOk(true);
     setPrix({ price });
-    // console.log(price);
+    console.log(price);
+    console.log(ok);
 
-    axios.post("http://localhost:3000/api/command/price", prix).then(res => {
-      // console.log(prix);
-    });
+    axios
+      .post("http://localhost:3000/api/command/price", { price })
+      .then(ok === true && <div className='popup'>blabla coute {price}</div>);
   };
 
   const { price } = props;
   return (
-    <div className='contain-checkbox' onClick={() => handleClick()}>
+    <div className='contain-checkbox'>
       <label className='lala'>
         <input className='input-card' type='checkbox' />
         <div className='card'>
-          <div className='frontT' />
+          <div className='frontT' onClick={() => handleClick()} />
           <div className='backT'>{price}</div>
         </div>
       </label>
